9f4f16b7ead6c0edfec32bfc90ee70e1868e044e
Dmitry Frolkov <dmitfrol152@mail.ru>
2026-04-09 15:40:05 +0300

    chore: add workflow deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import svgr from 'vite-plugin-svgr';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  base: '/frontend-challenge/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
