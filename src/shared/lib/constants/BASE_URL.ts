c155fa9c91907e8174c6bc2aa7f522870d1bc3b7
Dmitry Frolkov <dmitfrol152@mail.ru>
2026-04-09 18:23:11 +0300

    fix: base url

--- src/shared/lib/constants/BASE_URL.ts
+++ src/shared/lib/constants/BASE_URL.ts
@@ -1,2 +1,3 @@
-export const BASE_URL =
-  import.meta.env.VITE_API_BASE_URL ?? 'https://api.thecatapi.com/v1';
+export const BASE_URL = import.meta.env.VITE_API_BASE_URL
+  ? import.meta.env.VITE_API_BASE_URL
+  : 'https://api.thecatapi.com/v1';
